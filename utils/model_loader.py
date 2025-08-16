2dbd378cf9e2d974c9392e9719daa4a736ab29e4
Ravi7136 <ravi.ravirla@gmail.com>
2025-08-16 20:07:27 +0530

    files update

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,13 +1,15 @@
+
 import os
 import sys
 from dotenv import load_dotenv
 from utils.config_loader import load_config
+from .config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 #from langchain_openai import ChatOpenAI
 from logger.custom_logger import CustomLogger
-from exception.custom_exception_archieve import DocumentPortalException
+from exception.custom_exception import DocumentPortalException
 log = CustomLogger().get_logger(__name__)
 
 class ModelLoader:
